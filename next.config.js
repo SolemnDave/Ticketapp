459615ea0971f321ba4af6f6b276a754f5784d6c
SolemnDave <davidrjr94@gmail.com>
2024-06-19 11:51:49 -0400

    Changing next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+};
 
 module.exports = nextConfig;
